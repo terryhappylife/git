86ba8927e2092db69d53636896f59d99f7cd5143
terry <terry@163.com>
2021-01-13 21:13:44 +0800

    hot_fix commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hot_fix");
+        System.out.println("hot_fix");
     }
 }
